dfb54a215a3cc0ea8bcd3be7f03d174e77ff9ad5
German Abelentsev <german@abelentsev.pro>
2025-09-28 23:58:42 +0300

    [WIP] дописать всё кроме радиуса

--- Main.py
+++ Main.py
@@ -6,3 +6,12 @@ import json
 
 with open('atmosphere.json', 'r', encoding='utf-8') as f:
     atmo = json.load(f)
+pi = math.pi
+R = 8.314462618
+
+def radius(temp, pressure, nu, sigma):
+    q = nu * R * temp
+    Ds = np.sqrt(q * (81 * pressure ** 2 * q - 1024 * pi * sigma ** 3) / (576 * pi ** 2 * pressure ** 4))
+    alpha = np.cbrt(3 * q / (8 * pi * pressure) - 64 * sigma ** 3 / (27 * pressure ** 3) + Ds)
+    beta = np.cbrt(3 * q / (8 * pi * pressure) - 64 * sigma ** 3 / (27 * pressure ** 3) - Ds)
+    return alpha + beta - 4 * sigma / (3 * pressure)
